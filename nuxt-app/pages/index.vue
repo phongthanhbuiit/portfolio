0f39791b034c614df91d5fe2338315ed36b3be0f
Thompson <phongthanhbuiit@gmail.com>
2025-04-13 13:14:28 +0700

    phong update

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -2,138 +2,96 @@
   <section class="min-h-screen flex items-center justify-center bg-gray-900">
     <div class="w-full px-4 md:px-5 lg:px-5 mx-auto">
       <div
-        class="w-full md:px-16 px-10 md:pt-16 pt-10 pb-10 bg-gray-900 rounded-2xl flex-col justify-end items-center lg:gap-28 md:gap-16 gap-10 inline-flex"
-      >
+        class="w-full md:px-16 px-10 md:pt-16 pt-10 pb-10 bg-gray-900 rounded-2xl flex-col justify-end items-center lg:gap-28 md:gap-16 gap-10 inline-flex">
         <div class="flex-col justify-end items-center lg:gap-16 gap-10 flex">
-            <div class="text-center">
-              <h1 class="text-6xl font-black tracking-tight mb-2">
-                <span
-                  class="inline-block bg-gradient-to-r from-emerald-400 via-cyan-400 to-blue-500 bg-clip-text text-transparent"
-                >
-                  Welcome to My Website
-                </span>
-              </h1>
-              <div class="flex text-white  items-center justify-center gap-2">
-                <p class="text-lg">I'm</p>
-                <p class="text-lg">Bui Thanh Phong</p>
-                <p class="text-lg">(Thompson Bui)</p>
-              </div>
-              <p class="text-emerald-200 font-medium mt-2">
-                Fullstack Developer & Software Engineer
+          <div class="text-center">
+            <h1 class="text-4xl font-black tracking-tight mb-2">
+              <span
+                class="inline-block bg-gradient-to-r from-emerald-400 via-cyan-400 to-blue-500 bg-clip-text text-transparent">
+                I'm Thompson Bui
+              </span>
+            </h1>
+            <div class="flex items-center justify-center space-x-4 text-emerald-300">
+              <div class="h-px w-16 bg-emerald-400/30" />
+              <p class="text-lg font-medium"> Fullstack Developer & Software Engineer
               </p>
+              <div class="h-px w-16 bg-emerald-400/30" />
             </div>
+          </div>
           <div class="flex-col justify-center items-center gap-10 flex">
             <div class="flex-col justify-start items-center gap-2.5 flex">
-              <h2
-                class="text-center text-emerald-400 md:text-6xl text-5xl font-bold font-manrope leading-normal"
-              >
+              <h2 class="text-center text-emerald-400 md:text-6xl text-5xl font-bold font-manrope leading-normal">
                 Coming Soon
               </h2>
-              <p
-                class="text-center text-gray-500 text-base font-normal leading-relaxed"
-              >
+              <p class="text-center text-gray-500 text-base font-normal leading-relaxed">
                 Just {{ daysRemaining }} days remaining until the big reveal of
                 our new portfolio!
               </p>
             </div>
-            <div
-              class="flex items-start justify-center w-full gap-2 count-down-main"
-            >
+            <div class="flex items-start justify-center w-full gap-2 count-down-main">
               <div class="timer flex flex-col gap-0.5">
                 <div>
-                  <h3
-                    class="text-center text-white text-2xl font-bold font-manrope leading-9"
-                  >
+                  <h3 class="text-center text-white text-2xl font-bold font-manrope leading-9">
                     {{ countdown.days }}
                   </h3>
                 </div>
-                <p
-                  class="text-center text-gray-500 text-xs font-normal leading-normal w-full"
-                >
+                <p class="text-center text-gray-500 text-xs font-normal leading-normal w-full">
                   DAYS
                 </p>
               </div>
-              <h3
-                class="w-3 text-center text-gray-500 text-2xl font-medium font-manrope leading-9"
-              >
+              <h3 class="w-3 text-center text-gray-500 text-2xl font-medium font-manrope leading-9">
                 :
               </h3>
               <div class="timer flex flex-col gap-0.5">
                 <div>
-                  <h3
-                    class="text-center text-white text-2xl font-bold font-manrope leading-9"
-                  >
+                  <h3 class="text-center text-white text-2xl font-bold font-manrope leading-9">
                     {{ countdown.hours }}
                   </h3>
                 </div>
-                <p
-                  class="text-center text-gray-500 text-xs font-normal leading-normal w-full"
-                >
+                <p class="text-center text-gray-500 text-xs font-normal leading-normal w-full">
                   HRS
                 </p>
               </div>
-              <h3
-                class="w-3 text-center text-gray-500 text-2xl font-medium font-manrope leading-9"
-              >
+              <h3 class="w-3 text-center text-gray-500 text-2xl font-medium font-manrope leading-9">
                 :
               </h3>
               <div class="timer flex flex-col gap-0.5">
                 <div>
-                  <h3
-                    class="text-center text-white text-2xl font-bold font-manrope leading-9"
-                  >
+                  <h3 class="text-center text-white text-2xl font-bold font-manrope leading-9">
                     {{ countdown.minutes }}
                   </h3>
                 </div>
-                <p
-                  class="text-center text-gray-500 text-xs font-normal leading-normal w-full"
-                >
+                <p class="text-center text-gray-500 text-xs font-normal leading-normal w-full">
                   MINS
                 </p>
               </div>
-              <h3
-                class="w-3 text-center text-gray-500 text-2xl font-medium font-manrope leading-9"
-              >
+              <h3 class="w-3 text-center text-gray-500 text-2xl font-medium font-manrope leading-9">
                 :
               </h3>
               <div class="timer flex flex-col gap-0.5">
                 <div>
-                  <h3
-                    class="text-center text-white text-2xl font-bold font-manrope leading-9"
-                  >
+                  <h3 class="text-center text-white text-2xl font-bold font-manrope leading-9">
                     {{ countdown.seconds }}
                   </h3>
                 </div>
-                <p
-                  class="text-center text-gray-500 text-xs font-normal leading-normal w-full"
-                >
+                <p class="text-center text-gray-500 text-xs font-normal leading-normal w-full">
                   SECS
                 </p>
               </div>
             </div>
             <div class="w-full flex-col justify-center items-center gap-5 flex">
-              <h6
-                class="text-center text-emerald-400 text-base font-semibold leading-relaxed"
-              >
+              <h6 class="text-center text-emerald-400 text-base font-semibold leading-relaxed">
                 Launched Date: May 01, 2025
               </h6>
-              <div
-                class="justify-center items-center gap-2.5 flex sm:flex-row flex-col"
-              >
+              <div class="justify-center items-center gap-2.5 flex sm:flex-row flex-col">
                 <input
-                  v-model="email"
-                  type="text"
+v-model="email" type="text"
                   class="w-80 focus:outline-none px-3.5 py-2 shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] text-gray-900 placeholder-gray-400 text-sm font-normal leading-relaxed h-10 bg-white rounded-lg border border-gray-200 justify-start items-center gap-1.5 inline-flex"
-                  placeholder="Type your mail..."
-                >
+                  placeholder="Type your mail...">
                 <button
                   class="sm:w-fit w-full px-3.5 py-2 bg-emerald-400 hover:bg-emerald-600 transition-all duration-700 ease-in-out rounded-lg shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] justify-center items-center flex"
-                  @click="notifyMe"
-                >
-                  <span
-                    class="px-1.5 text-gray-900 text-sm font-medium leading-6 whitespace-nowrap"
-                    >Notify Me</span
-                  >
+                  @click="notifyMe">
+                  <span class="px-1.5 text-gray-900 text-sm font-medium leading-6 whitespace-nowrap">Notify Me</span>
                 </button>
               </div>
             </div>
@@ -142,10 +100,8 @@
         <p class="text-center text-gray-500 text-sm font-normal leading-snug">
           Get in touch with us:
           <a
-            href="mailto:phongthanhbuiit@gmail.com"
-            class="hover:text-gray-100 transition-all duration-700 ease-in-out"
-            >phongthanhbuiit@gmail.com</a
-          >
+href="mailto:phongthanhbuiit@gmail.com"
+            class="hover:text-gray-100 transition-all duration-700 ease-in-out">thompsonbui.dev@gmail.com</a>
         </p>
       </div>
     </div>
